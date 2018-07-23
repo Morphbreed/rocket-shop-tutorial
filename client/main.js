e2ac3ad609ff1633801a4ba5e64d1142d86a7d4d
Matthias Jaenicke <morphbreed@chello.at>
2018-07-23 19:33:43 +0200

    added showdown, added product page

--- client/main.js
+++ client/main.js
@@ -1,10 +1,13 @@
 import '../lib/router'
 import './main.html'
 import * as accounting from './scripts/accounting'
+import * as showdown from './scripts/showdown'
    
 UI.registerHelper("money", function(amount) {
     return accounting.formatMoney(amount);
+})   
+
+UI.registerHelper("markdown", function(text) {
+    var converter = new showdown.Converter();
+    return converter.makeHtml(text);
 })
-
-
-
